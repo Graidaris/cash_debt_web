d6d9946dcaa6f8c67c6fe1d837388d6a39a6aa2b
Graidaris <vladislavby96@gmail.com>
2020-01-21 14:38:09 +0100

    add flask_login

--- cash_debt_web/__init__.py
+++ cash_debt_web/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database_v1.db'
 app.config["SECRET_KEY"] = "OCML3BRawWEUeaxcuKHLpw"
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app=app)
 
 import cash_debt_web.models
 import cash_debt_web.routes
